40faa063bc8efe8438436308a0034558de4d6584
DranoelMit <tim.matthew.leonard@gmail.com>
2018-03-06 00:13:41 -0500

    Add basic cursor functionality, Bug w/ enter and current line width (need to work on backend first)

---
+++ public/JS/cursor.js
@@ -0,0 +1,35 @@
+const PAGE_X= 170;
+const PAGE_Y = 55;
+const LINE_WIDTH = 1320;
+const LINE_HEIGHT = 23.5;
+const CHAR_WIDTH = 8.81;
+
+var $cursor = $("#cursor");
+var cursorPos = {
+     x: PAGE_X,
+     y: PAGE_Y
+};
+
+function updateCursor(xRate){
+     cursorPos.x += xRate*CHAR_WIDTH;
+
+     if(cursorPos.x < PAGE_X){
+          if(cursorPos.y > PAGE_Y)
+               cursorPos.x = PAGE_X+LINE_WIDTH-CHAR_WIDTH;
+          else
+               cursorPos.x = PAGE_X;
+          cursorPos.y -= LINE_HEIGHT;
+          if(cursorPos.y < PAGE_Y){
+               cursorPos.y = PAGE_Y;
+          }
+     }
+     else if(cursorPos.x >= LINE_WIDTH+PAGE_X)
+     {
+          cursorPos.x = PAGE_X;
+          cursorPos.y += LINE_HEIGHT;
+     }
+
+     $cursor.css("top", cursorPos.y+"px");
+     $cursor.css("left", cursorPos.x+"px");
+     console.log("x: "+cursorPos.x+", y: "+cursorPos.y);
+}
--- public/JS/socket.js
+++ public/JS/socket.js
@@ -2,27 +2,40 @@ var socket = io.connect();
 var $page = $("#page");
 var $document = $(document);
 /* Tab, Space, backSpace, control */
-const SPECIAL_KEYS =[9, 32, 8, 17];
+const SPECIAL_KEYS =[9, 32, 8, 17, 13];
 
 $(window).load(function(){
      socket.emit("get text");
+     updateCursor(0);
 });
 $document.keydown(function(e){
      if(SPECIAL_KEYS.includes(e.keyCode)){
           e.preventDefault();
-     if(e.keyCode == 32)
-          socket.emit("new char"," ");
-     else if(e.keyCode == 9)
-          socket.emit("new char", "\t");
+          if(e.keyCode == 32){
+               socket.emit("new char"," ");
+               updateCursor(1);
+          }
+          else if(e.keyCode == 9){
+                    socket.emit("new char", "       ");
+               updateCursor(7);
+          }
+          else if(e.keyCode == 13){
+               socket.emit("new char", "\n");
+               updateCursor(150);
+          }
+
      }
 
-     if(e.keyCode == 8) //backspace
+     if(e.keyCode == 8){ //backspace
           socket.emit("rem char");
+          updateCursor(-1);
+     }
 
 });
 $document.keypress(function(e){
      e.preventDefault();
      socket.emit("new char",String.fromCharCode(e.keyCode));
+     updateCursor(1);
 });
 
 socket.on("soft update text", function(char){
