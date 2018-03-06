f0170540ef707923f9ad38c8e84cadc96f6645cb
DranoelMit <tim.matthew.leonard@gmail.com>
2018-03-06 09:21:15 -0500

    add arrow keys

--- public/JS/cursor.js
+++ public/JS/cursor.js
@@ -5,30 +5,38 @@ const LINE_HEIGHT = 23.5;
 const CHAR_WIDTH = 8.81;
 
 var $cursor = $("#cursor");
+
 var cursorPos = {
      x: PAGE_X,
      y: PAGE_Y
 };
 
-function updateCursor(xRate){
-     cursorPos.x += xRate*CHAR_WIDTH;
+function updateCursor(xRate, yRate){
+     if(xRate !=0 && yRate ==0){
+          cursorPos.x += xRate*CHAR_WIDTH;
 
-     if(cursorPos.x < PAGE_X){
-          if(cursorPos.y > PAGE_Y)
-               cursorPos.x = PAGE_X+LINE_WIDTH-CHAR_WIDTH;
-          else
+          if(cursorPos.x < PAGE_X){
+               if(cursorPos.y > PAGE_Y)
+                    cursorPos.x = PAGE_X+LINE_WIDTH-CHAR_WIDTH;
+               else
+                    cursorPos.x = PAGE_X;
+               cursorPos.y -= LINE_HEIGHT;
+               if(cursorPos.y < PAGE_Y){
+                    cursorPos.y = PAGE_Y;
+               }
+          }
+          else if(cursorPos.x >= LINE_WIDTH+PAGE_X)
+          {
                cursorPos.x = PAGE_X;
-          cursorPos.y -= LINE_HEIGHT;
+               cursorPos.y += LINE_HEIGHT;
+          }
+     }
+     else if(xRate == 0 && yRate != 0){
+          cursorPos.y += yRate*LINE_HEIGHT;
           if(cursorPos.y < PAGE_Y){
                cursorPos.y = PAGE_Y;
           }
      }
-     else if(cursorPos.x >= LINE_WIDTH+PAGE_X)
-     {
-          cursorPos.x = PAGE_X;
-          cursorPos.y += LINE_HEIGHT;
-     }
-
      $cursor.css("top", cursorPos.y+"px");
      $cursor.css("left", cursorPos.x+"px");
      console.log("x: "+cursorPos.x+", y: "+cursorPos.y);
--- public/JS/socket.js
+++ public/JS/socket.js
@@ -1,41 +1,53 @@
 var socket = io.connect();
 var $page = $("#page");
 var $document = $(document);
-/* Tab, Space, backSpace, control */
-const SPECIAL_KEYS =[9, 32, 8, 17, 13];
+/* Tab, Space, backSpace, control, right, left, up, down */
+const SPECIAL_KEYS =[9, 32, 8, 17, 13, 39, 37, 38, 40];
 
 $(window).load(function(){
      socket.emit("get text");
-     updateCursor(0);
+     updateCursor(0,0);
 });
 $document.keydown(function(e){
      if(SPECIAL_KEYS.includes(e.keyCode)){
           e.preventDefault();
           if(e.keyCode == 32){
                socket.emit("new char"," ");
-               updateCursor(1);
+               updateCursor(1,0);
           }
           else if(e.keyCode == 9){
                     socket.emit("new char", "       ");
-               updateCursor(7);
+               updateCursor(7,0);
           }
           else if(e.keyCode == 13){
                socket.emit("new char", "\n");
-               updateCursor(150);
+               updateCursor(150,0);
+          }
+          else if(e.keyCode == 39){
+               updateCursor(1,0);
+          }
+          else if(e.keyCode == 37){
+               updateCursor(-1,0);
+          }
+          else if(e.keyCode == 38){
+               updateCursor(0, -1);
+          }
+          else if(e.keyCode == 40){
+               updateCursor(0, 1);
           }
 
      }
 
      if(e.keyCode == 8){ //backspace
           socket.emit("rem char");
-          updateCursor(-1);
+          updateCursor(-1,0);
      }
 
 });
 $document.keypress(function(e){
      e.preventDefault();
      socket.emit("new char",String.fromCharCode(e.keyCode));
-     updateCursor(1);
+     updateCursor(1,0);
 });
 
 socket.on("soft update text", function(char){
